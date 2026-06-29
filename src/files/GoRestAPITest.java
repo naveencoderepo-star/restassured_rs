400c90620f6c2502bddf4f2cd03d6849fd8d61cc
naveencoderepo-star <naveencoderepo@users.noreply.github.com>
2026-06-29 18:59:55 +0530

    feat - added clean code with create user

--- src/files/GoRestAPITest.java
+++ src/files/GoRestAPITest.java
@@ -1,13 +1,10 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import org.testng.annotations.Test;
-
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
-/**
- * GoRest API - Testing with Timestamp-based Dynamic Email
- * API: https://gorest.in/public/v2/users
- * Authorization: Bearer demo-token
- */
+
 public class GoRestAPITest {
 
     @Test
@@ -24,7 +21,21 @@ public class GoRestAPITest {
                 .then().log().all().statusCode(201).extract().response().asString();
 
 
-        System.out.println("Newly created user : " + response);
+        JsonPath createUserResponse = new JsonPath(response);
+        String generatedId = String.valueOf(createUserResponse.getInt("id"));
+
+
+        given().header("Content-Type", "application/json").header("Authorization", "Bearer demo-token")
+
+                .when().delete("/users/" + generatedId)
+                .then().log().all().statusCode(204);
+
+        System.out.println("Deleted User ID: " + generatedId);
+
+
+        given().header("Content-Type", "application/json").header("Authorization", "Bearer demo-token")
+                .when().get("/users/" + generatedId)
+                .then().log().all().statusCode(404).assertThat().body("message", equalTo("Resource not found"));
 
 
     }
