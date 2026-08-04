db5f5f7cb8aa66e5f44a2f5cf3707b7563f614e9
naveencoderepo-star <naveencoderepo@users.noreply.github.com>
2026-08-04 19:27:11 +0530

    feat - added latest code after 7.30

--- src/(default package)/SpecBuilderTest.java
+++ src/(default package)/SpecBuilderTest.java
@@ -1,14 +1,14 @@
-
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.config.RedirectConfig;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import java.util.ArrayList;
-import java.util.List;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 
+import java.util.ArrayList;
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 
@@ -18,10 +18,10 @@ public class SpecBuilderTest {
 
     public static void main(String[] args) {
 
-        RestAssured.baseURI = "https://rahulshettyacademy.com/";
-
+//        RestAssured.baseURI = "https://rahulshettyacademy.com/";    we are building the base url in request spec builder using setBaseUri
 
         AddPlace p = new AddPlace();
+
         p.setAccuracy(50);
         p.setAddress("206, first floor");
         p.setLanguage("Tamil");
@@ -37,13 +37,11 @@ public class SpecBuilderTest {
         l.setLng(33.427362);
         p.setLocation(l);
 
+        RequestSpecification requestSpecBuilder = new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com/").addQueryParam("key", "qaclick123").setBody(p).build();
 
-        RequestSpecification requestSpec = new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com").addQueryParam("key", "qaclick123").setContentType(ContentType.JSON).build();
-        RequestSpecification res = given().spec(requestSpec).body(p);
+        ResponseSpecification responseSpecBuilder = new ResponseSpecBuilder().expectStatusCode(200).expectContentType(ContentType.JSON).build();
 
-        ResponseSpecification responseSpec = new ResponseSpecBuilder().expectStatusCode(200).expectContentType(ContentType.JSON).build();
-
-        Response response = res.when().post("/maps/api/place/add/json").then().spec(responseSpec).extract().response();
+        Response response = given().spec(requestSpecBuilder).body(p).config(RestAssured.config().redirect(RedirectConfig.redirectConfig().followRedirects(true))).when().post("maps/api/place/add/json").then().spec(responseSpecBuilder).extract().response();
 
         String responseString = response.asString();
         System.out.println(responseString);
