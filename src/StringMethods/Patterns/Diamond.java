80cf6af169161adf06e097002caf32cf031335f2
naveencoderepo-star <naveencoderepo@users.noreply.github.com>
2026-08-17 12:43:42 +0530

    feat - added patter packages and pyramidpatter classes

---
+++ src/StringMethods/Patterns/Diamond.java
@@ -0,0 +1,4 @@
+package StringMethods.Patterns;
+
+public class Diamond {
+}
---
+++ src/StringMethods/Patterns/IncreasingTriangle.java
@@ -0,0 +1,16 @@
+package StringMethods.Patterns;
+
+public class IncreasingTriangle {
+
+    public static void main(String[] args) {
+
+        int n = 5;
+
+
+        for (int i = 1; i <= n; i++) {
+
+
+
+        }
+    }
+}
